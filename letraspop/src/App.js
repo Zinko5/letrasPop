87108bcdf259f25e1495c96c665aa29c96ba1a33
Zinko <gabriel.marcelo.munoz@gmail.com>
2024-02-07 19:07:47 -0400

    crea el proyecto

---
+++ letraspop/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>a</body>
+    </div>
+  );
+}
+
+export default App;
---
+++ letraspop/src/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header>
+            <div>[LOGO]</div>
+            <div>Inicio</div>
+            <div>Descubrir</div>
+            <div>Buscar</div>
+        </header>
+    );
+}
+
+export default Header;
